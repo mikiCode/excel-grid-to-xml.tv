e002ceb868771b116e75dbd6f77b1f77d7b214b4
mikiCode <mikill.priv@gmail.com>
2023-07-04 20:47:37 +0200

    Added programme start and programm end dates with pandas in format suited for the xmltv

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, File, HTTPException, UploadFile
 from pydantic import BaseModel
 import pandas as pd
 from grid.schema import excel_columns
+import datetime as dt
 
 
 # create app
@@ -18,8 +19,21 @@ async def grid_upload(file: bytes = File(...)):
         df = pd.read_excel(file)
     except ValueError:
         raise HTTPException(400, detail="Invalid file type")
+
     # choosing columns to keep, hardcoded in schema.py
     df = df[excel_columns]
+    # convert datetime columns to strings with format required by xmltv - yyyymmddhhmmss
+    df["Start Time"] = df["Start Time"].dt.strftime("%H%M%S")
+    df["Date"] = df["Date"].dt.strftime("%Y%m%d")
+    df["Duration"] = df["Duration"].dt.strftime("%H:%M:%S")
+    # concatenating columns df["Date"] and df["Start Time"] to get column with format yyyymmddhhmmss
+    df["programme start"] = df["Date"] + df["Start Time"]
+    # create a column for the end time of the programme, no other way to retain 24h handling without hardcoding it
+    df["programme end"] = (
+        pd.to_datetime(df["programme start"], format="%Y%m%d%H%M%S")
+        + pd.to_timedelta(df["Duration"])
+    ).dt.strftime("%Y%m%d%H%M%S")
+
     # export pandas dataframe to json with channel and date range in filename
     grid_name = (
         df["Channel Name"].iloc[0]
